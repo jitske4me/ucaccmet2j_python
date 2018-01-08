86a562f7215ddeca114d190d84d672fa306fe354
Jitske <jitskekoeleman@hotmail.com>
2018-01-08 15:21:46 +0100

    Did pt 2, updated comments pt 1

--- Koeleman_Ass3_code.py
+++ Koeleman_Ass3_code.py
@@ -16,36 +16,48 @@ with open('precipitation.json') as file:
 #               'station': 'GHCND:USW00093814', 'value': 0}]        
 
 
-## to do: open stations, find seattle and corresponding code 
+## Maybe automate: open stations, find seattle and corresponding code 
 
 
-import json
-
-with open('precipitation.json') as file:
-    info = json.load(file)
+####
+## Part 1
+####
     
 Seattle_station = []
 monthly_prec = 12*[0]  
+# Order is: year-month-day
 
+# For all entries in info, check if it's Seattle and add that to a seperate list
 for entry in range(len(info)):
     temp = info[entry]['station']
     
-    #select all entries for Seattle and make that a list and make a list with month-precipitation
+    #select all entries for Seattle and make that a list
     if temp == 'GHCND:US1WAKG0038':
         Seattle_station += [info[entry]]
+        ## Select the Month, by looking at every entry, then at the key 'date' 
+        ## and then taking the month part from the string
         month = int(info[entry]['date'][5:7] )
+        ## At loc month-1 (bc index starts at 0), add the precipitation for that day
         monthly_prec[month-1]+= info[entry]['value']
 
 print(monthly_prec)
 
-#Write to a json file        
+# Write to a json file        
 with open('monthly_prec.json', 'w') as file:
     json.dump(monthly_prec, file) #, indent=4)  
 
 
+####
+## Part 2
+####
 
-#year-month-day
+yearly_prec = sum(monthly_prec)
+rel_prec = 12 * [0]
 
+for i in range(len(monthly_prec)):
+    rel_prec[i] = round(monthly_prec[i]/yearly_prec, 2)
+
+print(rel_prec)
 
 
 
